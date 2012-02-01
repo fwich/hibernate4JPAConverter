jboss-developer/jboss-eap-quickstarts

add quickstart application for demonstrating use of Hibernate in Jboss AS7

hibernatedemo/src/main/java/org/jboss/as/quickstart/hibernatedemo/controller/MemberRegistration.java
@@ -1,0 +1,73 @@
+package org.jboss.as.quickstart.hibernatedemo.controller;
+
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateful;
+import javax.enterprise.event.Event;
+import javax.enterprise.inject.Model;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import org.hibernate.Session;
+
+import org.jboss.as.quickstart.hibernatedemo.model.Member;
+
+/**
+ * @author Madhumita Sadhukhan
+ */
+// The @Stateful annotation eliminates the need for manual transaction demarcation
+@Stateful
+// The @Model stereotype is a convenience mechanism to make this a request-scoped bean that has an
+// EL name
+// Read more about the @Model stereotype in this FAQ:
+// http://sfwk.org/Documentation/WhatIsThePurposeOfTheModelAnnotation
+@Model
+public class MemberRegistration {
+
+   @Inject
+   private Logger log;
+
+   @Inject
+   private EntityManager em;
+   
+   @Inject
+   private Event<Member> memberEventSrc;
+   
+   private Member newMember;
+
+   @Produces
+   @Named
+   public Member getNewMember() {
+      return newMember;
+   }
+
+   @Produces
+   public void register() throws Exception {
+      log.info("Registering " + newMember.getName());
+      
+      //using Hibernate session(Native API) and JPA entitymanager
+      Session session = (Session)em.getDelegate();
+      session.persist(newMember);
+     
+      try
+      {
+      memberEventSrc.fire(newMember);
+      }
+      catch(Exception e)
+      {
+    	  log.info("Registration Failed!You have provided duplicate details for Member!!!");
+    	  e.printStackTrace();
+      }
+      
+      
+    	  
+      initNewMember();
+   }
+
+   @PostConstruct
+   public void initNewMember() {
+      newMember = new Member();
+   }
+}

hibernatedemo/src/main/java/org/jboss/as/quickstart/hibernatedemo/data/MemberListProducer.java
@@ -1,0 +1,51 @@
+package org.jboss.as.quickstart.hibernatedemo.data;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.jboss.as.quickstart.hibernatedemo.model.Member;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.event.Observes;
+import javax.enterprise.event.Reception;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+
+/**
+ * @author Madhumita Sadhukhan
+ */
+
+@RequestScoped
+public class MemberListProducer {
+   @Inject
+   private EntityManager em;
+   
+   private List<Member> members;
+
+   // @Named provides access the return value via the EL variable name "members" in the UI (e.g.,
+   // Facelets or JSP view)
+   @Produces
+   @Named
+   public List<Member> getMembers() {
+      return members;
+   }
+
+   public void onMemberListChanged(@Observes(notifyObserver = Reception.IF_EXISTS) final Member member) {
+      retrieveAllMembersOrderedByName();
+   }
+
+   @PostConstruct
+   public void retrieveAllMembersOrderedByName() {
+	   
+	  //using Hibernate Session and Criteria Query via Hibernate Native API 
+	  Session session = (Session) em.getDelegate();
+      Criteria cb = session.createCriteria(Member.class);
+      cb.addOrder(Order.asc("name"));
+      members = (List<Member>)cb.list();
+
+   }
+}

hibernatedemo/src/main/java/org/jboss/as/quickstart/hibernatedemo/model/Member.java
@@ -1,0 +1,92 @@
+package org.jboss.as.quickstart.hibernatedemo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/*The Model uses JPA Entity as well as Hibernate Validators
+ * @author Madhumita Sadhukhan
+ */
+
+@Entity
+@XmlRootElement
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "id"))
+public class Member implements Serializable {
+   /** Default value included to remove warning. Remove or modify at will. **/
+   private static final long serialVersionUID = 1L;
+
+   @Id
+   private Long id;
+
+   @NotNull
+   @Size(min = 1, max = 25)
+   @Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
+   private String name;
+
+   @NotNull
+   @NotEmpty
+   @Email
+   private String email;
+
+   @NotNull
+   @Size(min = 9, max = 12)
+   @Digits(fraction = 0, integer = 12)
+   @Column(name = "phone_number")
+   private String phoneNumber;
+   
+   private String address;
+
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public String getPhoneNumber() {
+      return phoneNumber;
+   }
+
+   public void setPhoneNumber(String phoneNumber) {
+      this.phoneNumber = phoneNumber;
+   }
+
+   public String getAddress() {
+	return address;
+   }
+
+   public void setAddress(String address) {
+	this.address = address;
+   }
+}

hibernatedemo/src/main/java/org/jboss/as/quickstart/hibernatedemo/util/Resources.java
@@ -1,0 +1,33 @@
+package org.jboss.as.quickstart.hibernatedemo.util;
+
+import java.util.logging.Logger;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * This class uses CDI to alias Java EE resources, such as the persistence context, to CDI beans
+ * 
+ * <p>
+ * Example injection on a managed bean field:
+ * </p>
+ * 
+ * <pre>
+ * &#064;Inject
+ * private EntityManager em;
+ * </pre>
+ */
+public class Resources {
+   // use @SuppressWarnings to tell IDE to ignore warnings about field not being referenced directly
+   @SuppressWarnings("unused")
+   @Produces
+   @PersistenceContext
+   private EntityManager em;
+
+   @Produces
+   public Logger produceLog(InjectionPoint injectionPoint) {
+      return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+   }
+}

hibernatedemo/src/main/resources/import.sql
@@ -1,0 +1,3 @@
+-- You can use this file to load seed data into the database using SQL statements
+insert into Member (id, name, email, phone_number, address) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212', 'Boston NY') 
+insert into Member (id, name, email, phone_number, address) values (1, 'Madhumita Sadhukhan', 'msadhukh@gmail.com', '2135551214', 'Brno CZ') 

fwich/hibernate4JPAConverter

src/main/java/org/jboss/as/quickstart/hibernate4/controller/MemberRegistration.java
@@ -1,0 +1,73 @@
+package org.jboss.as.quickstart.hibernate4.controller;
+
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateful;
+import javax.enterprise.event.Event;
+import javax.enterprise.inject.Model;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import org.hibernate.Session;
+
+import org.jboss.as.quickstart.hibernate4.model.Member;
+
+/**
+ * @author Madhumita Sadhukhan
+ */
+// The @Stateful annotation eliminates the need for manual transaction demarcation
+@Stateful
+// The @Model stereotype is a convenience mechanism to make this a request-scoped bean that has an
+// EL name
+// Read more about the @Model stereotype in this FAQ:
+// http://sfwk.org/Documentation/WhatIsThePurposeOfTheModelAnnotation
+@Model
+public class MemberRegistration {
+
+   @Inject
+   private Logger log;
+
+   @Inject
+   private EntityManager em;
+   
+   @Inject
+   private Event<Member> memberEventSrc;
+   
+   private Member newMember;
+
+   @Produces
+   @Named
+   public Member getNewMember() {
+      return newMember;
+   }
+
+   @Produces
+   public void register() throws Exception {
+      log.info("Registering " + newMember.getName());
+      
+      //using Hibernate session(Native API) and JPA entitymanager
+      Session session = (Session)em.getDelegate();
+      session.persist(newMember);
+     
+      try
+      {
+      memberEventSrc.fire(newMember);
+      }
+      catch(Exception e)
+      {
+    	  log.info("Registration Failed!You have provided duplicate details for Member!!!");
+    	  e.printStackTrace();
+      }
+      
+      
+    	  
+      initNewMember();
+   }
+
+   @PostConstruct
+   public void initNewMember() {
+      newMember = new Member();
+   }
+}

src/main/java/org/jboss/as/quickstart/hibernate4/data/MemberListProducer.java
@@ -1,0 +1,51 @@
+package org.jboss.as.quickstart.hibernate4.data;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.jboss.as.quickstart.hibernate4.model.Member;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.event.Observes;
+import javax.enterprise.event.Reception;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+
+/**
+ * @author Madhumita Sadhukhan
+ */
+
+@RequestScoped
+public class MemberListProducer {
+   @Inject
+   private EntityManager em;
+   
+   private List<Member> members;
+
+   // @Named provides access the return value via the EL variable name "members" in the UI (e.g.,
+   // Facelets or JSP view)
+   @Produces
+   @Named
+   public List<Member> getMembers() {
+      return members;
+   }
+
+   public void onMemberListChanged(@Observes(notifyObserver = Reception.IF_EXISTS) final Member member) {
+      retrieveAllMembersOrderedByName();
+   }
+
+   @PostConstruct
+   public void retrieveAllMembersOrderedByName() {
+	   
+      //using Hibernate Session and Criteria Query via Hibernate Native API 
+      Session session = (Session) em.getDelegate();
+      Criteria cb = session.createCriteria(Member.class);
+      cb.addOrder(Order.asc("name"));
+      members = (List<Member>)cb.list();
+
+   }
+}

src/main/java/org/jboss/as/quickstart/hibernate4/model/Member.java
@@ -1,0 +1,97 @@
+package org.jboss.as.quickstart.hibernate4.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/*use hibernate4 specific validators
+ *please note for hibernate3 these imports will differ 
+ *for eg import org.hibernate.validator.Email
+*/
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/*The Model uses JPA Entity as well as Hibernate Validators
+ * 
+ */
+
+@Entity
+@XmlRootElement
+@Table(name = "MemberHibernate4Demo",uniqueConstraints = @UniqueConstraint(columnNames = "id"))
+public class Member implements Serializable {
+   /** Default value included to remove warning. Remove or modify at will. **/
+   private static final long serialVersionUID = 1L;
+
+   @Id
+   private Long id;
+
+   @NotNull
+   @Size(min = 1, max = 25)
+   @Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
+   private String name;
+
+   /**using hibernate4 validators**/
+   @NotNull
+   @NotEmpty
+   @Email
+   private String email;
+
+   @NotNull
+   @Size(min = 9, max = 12)
+   @Digits(fraction = 0, integer = 12)
+   @Column(name = "phone_number")
+   private String phoneNumber;
+   
+   private String address;
+
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public String getPhoneNumber() {
+      return phoneNumber;
+   }
+
+   public void setPhoneNumber(String phoneNumber) {
+      this.phoneNumber = phoneNumber;
+   }
+
+   public String getAddress() {
+	return address;
+   }
+
+   public void setAddress(String address) {
+	this.address = address;
+   }
+}

src/main/java/org/jboss/as/quickstart/hibernate4/util/Resources.java
@@ -1,0 +1,33 @@
+package org.jboss.as.quickstart.hibernate4.util;
+
+import java.util.logging.Logger;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * This class uses CDI to alias Java EE resources, such as the persistence context, to CDI beans
+ * 
+ * <p>
+ * Example injection on a managed bean field:
+ * </p>
+ * 
+ * <pre>
+ * &#064;Inject
+ * private EntityManager em;
+ * </pre>
+ */
+public class Resources {
+   // use @SuppressWarnings to tell IDE to ignore warnings about field not being referenced directly
+   @SuppressWarnings("unused")
+   @Produces
+   @PersistenceContext
+   private EntityManager em;
+
+   @Produces
+   public Logger produceLog(InjectionPoint injectionPoint) {
+      return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+   }
+}

src/main/resources/import.sql
@@ -1,0 +1,3 @@
+-- You can use this file to load seed data into the database using SQL statements
+insert into MemberHibernate4Demo (id, name, email, phone_number, address) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212', 'Boston NY') 
+insert into MemberHibernate4Demo (id, name, email, phone_number, address) values (1, 'Madhumita Sadhukhan', 'msadhukh@gmail.com', '2135551214', 'Brno CZ') 